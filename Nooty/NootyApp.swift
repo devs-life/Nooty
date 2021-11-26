1972a0851191f9b8b68735e813d09824014cc4a3
Ajay <ajaychoudhary19990@gmail.com>
2021-11-26 22:12:00 +0530

    first commit

---
+++ Nooty/NootyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NootyApp.swift
+//  Nooty
+//
+//  Created by Ajay on 26/11/21.
+//
+
+import SwiftUI
+
+@main
+struct NootyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ViewControllerView()
+        }
+    }
+}
